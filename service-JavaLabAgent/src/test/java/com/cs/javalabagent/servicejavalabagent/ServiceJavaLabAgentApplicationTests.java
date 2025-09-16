a529b524fb734ae6305f54e73c409c380e11bb80
caoshuai.cs <shuaicao01@163.com>
2025-09-16 15:52:40 +0800

    first

---
+++ service-JavaLabAgent/src/test/java/com/cs/javalabagent/servicejavalabagent/ServiceJavaLabAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.javalabagent.servicejavalabagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceJavaLabAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
